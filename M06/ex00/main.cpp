6cd140ef7d2db31b302f124cb73b2f6bbc4be209
Tariq El bouhali <tariqelbouhali039@gmqil.com>
2022-08-07 21:26:12 +0100

    0

--- M06/ex00/main.cpp
+++ M06/ex00/main.cpp
@@ -12,7 +12,6 @@ int	isNumber(std::string arg, int len)
 		i++;
 	while (i < len)
 	{
-		std::cout << arg[i] << "  " << isdigit(arg[i]) << std::endl;  
 		if (i == len - 1 && arg[i] == 'f')
 			return (0);
 		if (isdigit(arg[i]))
@@ -26,7 +25,6 @@ int	isNumber(std::string arg, int len)
 			i++;
 		else
 		{
-			std::cout << arg[i] << std::endl;
 			return (1);
 		}
 	}
@@ -38,20 +36,120 @@ void	convertToChar(const char * arg)
 {
 	int 		len;
 	std::string	temp;
+	int			nbr;
 	char		res;
 	
 	temp = arg;
 	len = temp.length();
-	if (isNumber(arg, len))
-		std::cout << "char: impossible" << std::endl;
-	res = atoi(arg);
-	std::cout  << "num  = " << (int) res << std::endl;
-	if (res > 31 && res < 127)
+	if (len == 1 && ((arg[0] > 32 && arg[0] < 48) || (arg[0] > 57 && arg[0] < 127)))
+		nbr	= (int) arg[0];
+	else if (isNumber(arg, len))
 	{
-		std::cout << "char: " << res << std::endl;
+		std::cout << "char: impossible" << std::endl;
+		return ;
 	}
 	else
-		std::cout << "char: non displlayable" << std::endl;
+		nbr = atoi(arg);	
+	if (nbr > 31 && nbr < 127)
+	{
+		res = nbr;
+		std::cout << "char: " << res << std::endl;
+		return ;
+	}
+	else
+		std::cout << "char: non displayable" << std::endl;
+}
+
+void	convertToInt(const char* arg)
+{
+	int			len;
+	std::string	temp;
+	int			res;
+
+	temp = arg;
+	len = temp.length();
+	if (isNumber(arg, len))
+	{
+		std::cout << "int: impossible" << std::endl;
+		return ;
+	}
+	res = atoi(arg);
+	std::cout << "int: " << res << std::endl;
+}
+
+float	getFraction(const char *arg)
+{
+	float 	nbr;
+	int		tmp;
+	int 	i;
+	int 	j;
+
+	i = 0;
+	nbr = 0.0f;
+	while (arg[i] && arg[i] != '.')
+		i++;
+	i++;
+	j = 0;
+	tmp = 0;
+	while (arg[i] && (arg[i]) >= '0' && arg[i] <= '9')
+	{
+		tmp = tmp * 10;
+		tmp += arg[i] - 48;
+		i++;
+		j++;
+	}
+	i = 1;
+	while (j--)
+	 	i = i * 10;
+	nbr = (float) tmp / (float)i;
+	return (nbr);
+}
+
+int	isFloat(const char * arg)
+{
+	int		i;
+	int		dec_point;
+	int		flag_point;
+	float	nbr;
+
+	flag_point = 0;
+	i = 0;
+	dec_point = 0;
+	if (arg[0] == '-' && arg[0] == '+')
+		i++;
+	while (arg[i])
+	{
+		if (arg[i] == '.')
+			flag_point = 1;
+		else if (flag_point && arg[i] != 'f')
+			dec_point++;
+		i++;
+	}
+	if (dec_point)
+	{
+		nbr = atoi(arg) + getFraction(arg);
+		std::cout << "float: " << nbr << "f"<< std::endl;
+	}
+	return (dec_point);
+}
+
+void	convertToFloat(const char * arg)
+{
+	std::string	temp;
+	int			len;
+
+	temp = arg;
+	len = temp.length();
+	if (isNumber(arg, len))
+	{
+		std::cout << "float: imposible" << std::endl;
+		return ;
+	}
+	if (isFloat(arg))
+	{
+
+		return ;
+	}
 }
 
 int	main(int ac, char **av)
@@ -62,7 +160,7 @@ int	main(int ac, char **av)
 		return (1);
 	}
 	convertToChar(av[1]);
-	//float a = -.f;
-	//std::cout << a << std::endl;
+	convertToInt(av[1]);
+	convertToFloat(av[1]);
 	return (0);
 }
