8a0071842379c5cfe226346dfd71d60643765fa1
tariqelb <tariqelbouhali039@gmail.com>
2022-09-11 13:11:49 +0100

    Cpp Modul07

--- M07/ex02/main.cpp
+++ M07/ex02/main.cpp
@@ -12,19 +12,89 @@
 
 #include <iostream>
 #include "Array.hpp"
+#include <complex>
+
+/*
+class Complex
+{
+	public:
+		int rel;
+		int img;
+
+	Complex(): rel(0), img(0) {}
+	Complex(int _rel, int _img): rel(_rel), img(_img) {}
+	Complex(const Complex& rhs)
+	{
+		*this = rhs;
+	}
+	Complex&	operator=(const Complex& rhs)
+	{
+		if (this != &rhs)
+		{
+			this->rel = rhs.rel;
+			this->img = rhs.img;
+		}
+		return (*this);
+	}
+	~Complex(){};
+	void display()
+	{
+		std::cout << "rel " << this->rel << "\nimg " << this->img << std::endl;
+	}
+	void add(Complex c1, Complex c2)
+	{
+		this->rel = c1.rel + c2.rel;
+		this->img = c1.img + c2.img;
+	}
+};
+
+std::ostream&	operator<<(std::ostream& o, const Complex& rhs)
+{
+	o << "(" << rhs.rel << ", ";
+	o << rhs.img << ")" << std::endl;
+	return (o);
+}
+
+
 
 int	main(void)
 {
-	Array<int> A(2);
+	Array<Complex> A(2);
 	int i;
 
+	i = 0;
 	try 
 	{
 		while (i < 3)
 		{
-			//A[i] = i;
+			Complex a(1, 2);
+			A[i] = a;
 			std::cout << A[i] << std::endl;
 			i++;
+		}
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	return (0);
+}
+*/
+
+int	main(void)
+{
+	Array<std::complex<int> > A(2);
+	int index;
+
+	index = 0;
+	try 
+	{
+		while (index < 3)
+		{
+			std::complex<int> z(1,2);
+			A[index] = z;
+			std::cout << A[index] << std::endl;
+			index++;
 		}
 	}
 	catch (std::exception& e)
